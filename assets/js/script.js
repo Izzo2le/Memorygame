f40f1db444dd3889b562967ae1d8ab87711b7b00
Izzo2le <isobel.otoole@aim.com>
2023-07-14 17:32:28 +0000

    scriptjs updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-const moves = document.getElemendbyId("moves-count")
+const moves = document.getElemendbyId("moves-count");
+const timeValue = document.getElemendbyId("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
